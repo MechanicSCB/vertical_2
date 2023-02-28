433ce3d60702d2b870e06fbaa11e34d0fc61df5b
Nikolay <kolek-ulyanov@ya.ru>
2023-02-28 21:50:03 +0300

    Add Product Tabs

---
+++ resources/js/Pages/Products/Partials/TabBookmark.vue
@@ -0,0 +1,19 @@
+<script setup>
+import {inject} from "vue";
+
+let props = defineProps({tabName: Object});
+
+let activeTab = inject('activeTab');
+</script>
+
+<template>
+    <li @click="activeTab=tabName"
+        :class="activeTab===tabName?'text-ui-text-accent':'border-b border-ui-text-accent border-dashed cursor-pointer'"
+    >
+        <slot/>
+    </li>
+</template>
+
+<style scoped>
+
+</style>
--- resources/js/Pages/Products/Show.vue
+++ resources/js/Pages/Products/Show.vue
@@ -4,6 +4,7 @@ import HeartIcon from "../../Svg/HeartIcon.vue";
 import {ref} from "vue";
 import CheckedIcon from "../../Svg/CheckedIcon.vue";
 import CursorArrowClickedIcon from "../../Svg/CursorArrowClickedIcon.vue";
+import ProductTabs from "./Partials/ProductTabs.vue";
 
 let showModalImg = ref(false);
 let showOrigSizeImg = ref(false);
@@ -26,6 +27,7 @@ let props = defineProps({
     </div>
 
     <div class="mt-24 mx-auto px-9 max-w-[1656px]">
+        <!--  IMAGE/BUY CARD  -->
         <div class="mt-10 flex md:flex-row flex-col gap-8">
             <!--  LEFT  -->
             <div class="xl:w-1/3 md:w-1/2 w-full">
@@ -52,15 +54,13 @@ let props = defineProps({
                     <div>Код товара: {{ product.code }}</div>
 
                     <div class="mt-3 flex gap-2">
-                        <div class="flex">
-                            <img v-for="star in [1,1,1,1,1]" src="/images/star.png" alt="">
-                        </div>
+                        <div class="flex"><img v-for="star in [1,1,1,1,1]" src="/images/star.png" alt=""></div>
                         <span class="review-label">{{ product.reply_count }} отзыв </span>
                     </div>
                 </div>
 
                 <!-- BUY CARD -->
-                <div class="px-6 py-6 rounded-3xl bg-ui-light text-ui-text-secondary">
+                <div class="mt-10 px-6 py-6 rounded-3xl bg-ui-light text-ui-text-secondary">
                     <!--  availability  -->
                     <div class="flex items-center text-ui-text-accent gap-6">
                         <CheckedIcon class="w-4 fill-ui-text-accent"/>
@@ -111,5 +111,117 @@ let props = defineProps({
             </div>
         </div>
 
+        <!-- PRODUCT TABS  -->
+        <ProductTabs :product="product"/>
+
+        <!-- ADDDDD -->
+        <div class="hidden tab-wrapper">
+            <div class="tab-header">
+                <a href="javascript:void(0)" data-target="#tab-reviews" class="js-att-tab"><span>Отзывы</span></a>
+            </div>
+            <div class="tab" id="tab-reviews">
+                <div class="reviews">
+                    <div class="reviews-head todo">
+                        <div class="grid-row">
+                            <div class="col col-xl-2 col-lg-2 col-md-3 col-sm-12">
+                                <div class="reviews-head-title">Отзывов: 75</div>
+                            </div>
+                            <div class="col col-xl-10 col-lg-10 col-md-9 col-sm-12">
+                                <div class="reviews-head-info">
+                                    <div class="sorter">
+                                        <span class="sorter-title">Сортировать:</span>
+                                        <a href="javascript:void(0)" class="active asc"><span>по новизне</span></a>
+                                        <a href="javascript:void(0)"><span>по оценке</span></a>
+                                        <a href="javascript:void(0)"><span>по пользе</span></a>
+                                    </div>
+                                    <select class="sorter">
+                                        <option value="" selected="" disabled="">Сортировать по:</option>
+                                        <option value="1">по новизне по возрастанию</option>
+                                        <option value="2">по новизне по убыванию</option>
+                                        <option value="3">по оценке по возрастанию</option>
+                                        <option value="4">по оценке по убыванию</option>
+                                        <option value="5">по пользе по возрастанию</option>
+                                        <option value="6">по пользе по убыванию</option>
+                                    </select>
+                                    <a href="javascript:void(0)" class="btn fill-lipstick"><span>Оставить отзыв</span></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="reviews-list">
+                    </div>
+                    <div class="wdr2_list" id="wdr2_list_jRlUXc">
+                        <div class="wdr2_items">
+                            <div id="wdr2_item_273">
+                                <div class="wdr2_item" id="bx_3566614279_273">
+                                    <a name="273"></a>
+                                    <div class="wdr2_baloon_body">
+                                        <div class="wdr2_item_rating">
+                                            <div id="wd_reviews2_rating_b4796f07e8803bbf44231e1c74efd1da" class="wd_reviews2_rating" style="white-space:nowrap" title=""><img alt="1" src="/upload/webdebug.reviews/1/star/active.png" title=""><img alt="2" src="/upload/webdebug.reviews/1/star/active.png" title=""><img alt="3" src="/upload/webdebug.reviews/1/star/active.png" title=""><img alt="4" src="/upload/webdebug.reviews/1/star/active.png" title=""><img alt="5" src="/upload/webdebug.reviews/1/star/active.png" title=""><input name="score" type="hidden" value="5" readonly=""></div>	<div class="wdr2_item_rating_detail">
+                                            <div class="wdr2_one_rating">
+                                                Рейтинг<br>
+                                                <div id="wd_reviews2_rating_1659cddcc2f72a6dd692665a53eac680" class="wd_reviews2_rating" style="white-space:nowrap" title=""><img alt="1" src="/upload/webdebug.reviews/1/star/active.png" title=""><img alt="2" src="/upload/webdebug.reviews/1/star/active.png" title=""><img alt="3" src="/upload/webdebug.reviews/1/star/active.png" title=""><img alt="4" src="/upload/webdebug.reviews/1/star/active.png" title=""><img alt="5" src="/upload/webdebug.reviews/1/star/active.png" title=""><input name="score" type="hidden" value="5" readonly=""></div>	</div>
+                                        </div>
+                                        </div>
+                                        <div class="wdr2_fields">
+                                            <div class="wdr2_field"><div class="wdr2_field_value" style="font-weight: bold;">Николай</div></div>
+                                            <div class="wdr2_field"><div class="wdr2_field_value">Хороший аппарат. Все сварочные работы выполняются отлично. Уже три года им пользуюсь, ни разу не понадобился ремонт. Сваривал металл от 1,5 мм до 12 мм разными типами электродов. Нареканий никаких нет. Только кабель коротковат, использую удлинитель. </div></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="wdr2_pager wdr2_pager_bottom"></div>
+                    </div>
+                    <br>
+                    <div class="wdr2_iframe_hidden" style="display:none"><iframe onload="wdr2_add_iframe_1_loaded(this);" src="about:blank" name="wdr2_add_iframe_1" id="wdr2_add_iframe_1" class="wdr2_iframe"></iframe></div>
+                    <div class="wdr2_add wdr2_add_1">
+                        <div class="base-form ready wdr2_form_wrapper">
+                            <div class="form_title">Оставить отзыв</div>
+                            <div class="wdr2_result"></div>
+                            <form name="wdr2_add_form_1" id="wdr2_add_form_1" target="wdr2_add_iframe_1" action="/bitrix/tools/wd_reviews2.php?action=save&amp;interface=1&amp;target=E_451855&amp;anticache=751559884" enctype="multipart/form-data" method="post">
+                                <div>
+                                    <div class="ratings">
+                                        <table>
+                                            <tbody>
+                                            <tr>
+                                                <td class="rating_title">Рейтинг:</td>
+                                                <td class="rating_value"><div id="wd_reviews2_rating_df8b9ba2cf410436c014f43c9e5f099f" class="wd_reviews2_rating" style="white-space: nowrap; cursor: pointer;"><img alt="1" src="/upload/webdebug.reviews/1/star/inactive.png" title=""><img alt="2" src="/upload/webdebug.reviews/1/star/inactive.png" title=""><img alt="3" src="/upload/webdebug.reviews/1/star/inactive.png" title=""><img alt="4" src="/upload/webdebug.reviews/1/star/inactive.png" title=""><img alt="5" src="/upload/webdebug.reviews/1/star/inactive.png" title=""><input name="review[RATINGS][1]" type="hidden"></div></td>
+                                            </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                    <div class="fields">
+                                        <div class="field_name">
+                                            <label>
+                                                <input type="text" name="review[FIELDS][NAME]" value="" id="wdr2_ujnvsq">
+                                                <span class="placeholder">Имя</span>
+                                            </label>
+                                        </div>
+                                        <div class="field_comment">
+                                            <label class="textarea-row">
+                                                <textarea name="review[FIELDS][COMMENT]" style="overflow:auto; resize:vertical; " class="textarea-row" id="wdr2_notlz6"></textarea>
+                                                <span class="placeholder">Комментарий</span>
+                                            </label>
+                                        </div>
+                                        <div class="field wdr2_reqfield"><input type="text" name="field_1" value=""></div>
+                                    </div>
+                                    <div class="submit">
+                                        <input class="btn fill-lipstick" type="submit" value="Отправить">
+                                    </div>
+                                    <input type="hidden" name="wdr2_interface" value="1">
+                                    <input type="hidden" name="wdr2_target" value="E_451855">
+                                    <input type="hidden" name="wdr2_form" value="1">
+                                    <input type="hidden" name="wdr2_field" value="review">
+                                    <input type="hidden" name="wdr2_site" value="s1">
+                                    <input type="hidden" name="wdr2_url" value="%2Fproduct%2Fsvarochnyy-apparat-resanta-saipa-165-mig-mag-poluavtomat-6200-vt%2F">
+                                    <input type="hidden" name="wd_reviews2_review_sessid" id="wd_reviews2_review_sessid" value="254f8cfd87014e13e6e6b93ab2441930">	</div>
+                            </form>
+                        </div>
+                    </div>
+                    <br><br>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
